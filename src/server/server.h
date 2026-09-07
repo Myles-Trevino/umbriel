44c1a0d931fe3cbb1d41b9ee963137550505d250
Gustavo Rosa <Gustav0ar@users.noreply.github.com>
2026-09-07 10:28:48 -0300

    feat(drm): add GPU exclusion policy (#106)
    * feat(drm): add GPU exclusion policy
    
    * fix(config): fail closed missing optional DRM includes
    
    ---------
    
    Co-authored-by: Lemmy <studio@quadbyte.net>

--- src/server/server.h
+++ src/server/server.h
@@ -98,6 +98,7 @@ namespace umbriel {
   struct Keybind;
 
   class Cursor;
+  class BackendManager;
   class FocusManager;
   class XwaylandSupervisor;
   class ConfigWatcher;
@@ -492,6 +493,7 @@ namespace umbriel {
     };
 
     wl_display* m_display = nullptr;
+    std::unique_ptr<BackendManager> m_backendManager;
     wlr_backend* m_backend = nullptr;
     wlr_session* m_session = nullptr;
     wlr_renderer* m_renderer = nullptr;
